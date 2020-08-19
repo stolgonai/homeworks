995a1f02a4af63ad54cfe53af9a30f7d38011afe
Tolgonai Serkebaeva <t.serkebaeva@gmail.com>
2020-08-19 19:34:24 -0400

    js-loop-arr-obj-hw

--- javascript/3-loops-arrays-objects/hw-2/scripts.js
+++ javascript/3-loops-arrays-objects/hw-2/scripts.js
@@ -1,1 +1,38 @@
 // Your JS code is here
+
+// // Top Choice
+// // Create an array to hold your top choices (colors, presidents, whatever). For each choice, log to the screen a string like: "My #1 choice is blue."
+
+// // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is. The method slice might be helpful here.
+
+// // Max, Salidat, Tolgonai, Rustam #5 Breakout Room
+// // console.log("work#2 -----------------------------")
+var myFavCom = ['Apple', 'Microsoft', 'Facebook', 'Amazon', 'Tesla', 'Instagram', 'Google','Apple', 'Microsoft', 'Facebook', 'Amazon','Apple', 'Microsoft'];
+
+function topChoice(array){
+  var st = 'st';
+  var nd = 'nd';
+  var rd = 'rd';
+  var th = 'th';
+
+  
+  for(var i = 0, orderNum=1; i<array.length; i++,orderNum++){
+
+    if(orderNum%10===1 && orderNum != 11){
+      console.log(`My ${orderNum+st} favorite company is: ${myFavCom[i]}`)
+      console.log(orderNum+st + " favorite company is:" + myFavCom[i])
+    }
+    else if(orderNum%10===2 && orderNum != 12){
+      console.log(`My ${orderNum+nd} favorite company is: ${myFavCom[i]}`)
+    }
+    else if(orderNum%10===3 && orderNum != 13){
+      console.log(`My ${orderNum+rd} favorite company is: ${myFavCom[i]}`)
+    }else{
+      console.log(`My ${orderNum+th} favorite company is: ${myFavCom[i]}`)
+
+    }
+
+  }
+}
+
+topChoice(myFavCom);
--- javascript/3-loops-arrays-objects/hw-3/scripts.js
+++ javascript/3-loops-arrays-objects/hw-3/scripts.js
@@ -1,1 +1,79 @@
 // Your JS code is here
+
+// // The Reading List
+// // Keep track of which books you read and which books you want to read!
+
+// // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+// // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+// console.log("work#3 -----------------------------")
+
+var myBooks = [
+    {
+      title: "To Kill a Mockingbird",
+      author:"Harper Lee",
+      alreadyRead: false,
+  
+    },
+    {
+      title: "Ceremony",
+      author: "Leslie Marmon Silko",
+      alreadyRead: false,
+  
+    },
+    {
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      alreadyRead: true,
+  
+    },
+    {
+      title: "Schindler’s List",
+      author: "Thomas Keneally",
+      alreadyRead: true,
+  
+    },
+    {
+      title: "Harry Potter and the Philosopher’s Stone",
+      author: "J.K. Rowling",
+      alreadyRead: true,
+  
+    },
+    {
+      title:"The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      alreadyRead: true,
+  
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      alreadyRead: false,
+  
+    }
+  ]
+  for(let i = 0; i < myBooks.length; i++){
+    for(let key in myBooks[i]){
+      if(key === "title"){
+        // console.log(`${myBooks[i]["title"]} by ${myBooks[i]["author"]}`)
+      }
+    }
+  }
+  
+  
+  for(let i = 0; i < myBooks.length; i++){
+    for(var key in myBooks[i]){
+      // console.log(myBooks[i][key])
+      if(key === "alreadyRead"){
+        if(myBooks[i][key] === true){
+          console.log("You are already read '" + myBooks[i]["title"] + "' by " + myBooks[i]["author"]) 
+        }
+         if(myBooks[i][key] !== true){
+          console.log("You still need to read '" + myBooks[i]["title"] + "' by " + myBooks[i]["author"]) 
+        }
+        // console.log(key, myBooks[i][key])
+      }  
+    }
+  }
+  
