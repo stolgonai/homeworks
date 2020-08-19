a47f74ffc401dcb451cc3cd08a65b836e59df03a
Tolgonai Serkebaeva <t.serkebaeva@gmail.com>
2020-08-19 18:12:57 -0400

    added exercise #5 and #6

--- javascript/2-functions/hw-5/scripts.js
+++ javascript/2-functions/hw-5/scripts.js
@@ -1,1 +1,13 @@
 // Your JS code is here
+function mixUp(str1, str2){
+    var result1 = str1.slice(0,2) + str2.slice(2);
+    var result2 = str2.slice(0, 2) + str1.slice(2);
+    return result2 + " " + result1
+   
+   }
+   
+   mixUp('mix', 'pod') //: 'pox mid'
+   mixUp('dog', 'dinner') //: 'dig donner'
+   
+   
+  
--- javascript/2-functions/hw-6/scripts.js
+++ javascript/2-functions/hw-6/scripts.js
@@ -1,1 +1,16 @@
 // Your JS code is here
+function fixStart(str) {
+    let firstCh = str[0];
+    var result = firstCh;
+    for(let i = 1; i < str.length; i++){
+       if(str[i] === firstCh){
+       result += "*";
+       continue;
+      }
+      result += str[i];
+    }
+      return result
+  
+  }
+  fixStart('babble') //: 'ba**le'
+  fixStart('turtle') //: 'tur*le'
