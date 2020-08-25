c159f551c14246335b80247d821df87d6d702248
Tolgonai Serkebaeva <t.serkebaeva@gmail.com>
2020-08-25 19:20:46 -0400

    intro to dom tolgonai

--- javascript/4-intro-to-dom/hw-2/scripts.js
+++ javascript/4-intro-to-dom/hw-2/scripts.js
@@ -1,1 +1,80 @@
 // Your JS code is here
+
+var myBooks = [
+    {
+      img: "t.ly/KhcU",
+      linkToRead: "https://www.barnesandnoble.com/w/to-kill-a-mockingbird-harper-lee/1100151011;jsessionid=003065C95E092A585010E5A9A75E0C1D.prodny_store01-atgap04?ean=9780446310789",
+      title: "To Kill a Mockingbird",
+      author:"Harper Lee",
+      alreadyRead: false,
+  
+    },
+    {
+      img: "t.ly/ZCR8",
+      linkToRead: "https://www.barnesandnoble.com/w/ceremony-leslie-marmon-silko/1100470410?ean=9780143104919",
+      title: "Ceremony",
+      author: "Leslie Marmon Silko",
+      alreadyRead: false,
+  
+    },
+    {
+      img: "t.ly/FQMv",
+      linkToRead: "https://www.barnesandnoble.com/w/moby-dick-melville-herman/1110282307?ean=9781593080181",
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      alreadyRead: true,
+  
+    },
+    {
+      img: "t.ly/lHtS",
+      linkToRead: "https://www.barnesandnoble.com/w/schindlers-list-thomas-keneally/1102298238?ean=9780671880316",
+      title: "Schindler’s List",
+      author: "Thomas Keneally",
+      alreadyRead: true,
+  
+    },
+    {
+      img: "t.ly/S7pp",
+      linkToRead: "barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-minalima-design/1136406156?ean=9781338596700",
+      title: "Harry Potter and the Philosopher’s Stone",
+      author: "J.K. Rowling",
+      alreadyRead: true,
+  
+    },
+    {
+      img: "t.ly/p3cX",
+      linkToRead: "https://www.barnesandnoble.com/w/the-great-gatsby-sparknotes/1104681310?ean=9781411469570",
+      title:"The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      alreadyRead: true,
+  
+    },
+    {
+      img: "./prideAndPred.jpg",
+      linkToRead: "https://www.barnesandnoble.com/w/barnes-noble-leatherbound-classics-pride-and-prejudice-jane-austen/1106658784?ean=9781435159631",
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      alreadyRead: false,
+  
+    }
+  ]
+  var bodyTag = document.querySelector("body")
+  
+            // FIRST VERSION
+  
+  for(let i = 0; i < myBooks.length; i++){
+      var  booksObj = myBooks[i]
+      for(let book in booksObj){
+          if(book === "title" ){
+              var titleEl = document.createElement("p");
+              titleEl.classList.add("bookTitle")
+              var authorEl = document.createElement("p");
+              // authorEl.classList.add("author")
+              titleEl.textContent = booksObj["title"] +", " + booksObj["author"];
+              // authorEl.textContent = booksObj["author"]
+              bodyTag.appendChild(titleEl)
+              // bodyTag.appendChild(authorEl)
+          }
+      }
+  }
+  
--- javascript/4-intro-to-dom/hw-3/scripts.js
+++ javascript/4-intro-to-dom/hw-3/scripts.js
@@ -1,1 +1,131 @@
 // Your JS code is here
+let myBooks = [
+    {
+      img:
+        "https://m.media-amazon.com/images/I/81aY1lxk+9L._AC_UY545_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/to-kill-a-mockingbird-harper-lee/1100151011;jsessionid=003065C95E092A585010E5A9A75E0C1D.prodny_store01-atgap04?ean=9780446310789",
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      alreadyRead: false
+    },
+    {
+      img:
+        "https://m.media-amazon.com/images/I/71PZNtvJNEL._AC_UY545_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/ceremony-leslie-marmon-silko/1100470410?ean=9780143104919",
+      title: "Ceremony",
+      author: "Leslie Marmon Silko",
+      alreadyRead: false
+    },
+    {
+      img:
+        "https://m.media-amazon.com/images/I/81HBvFY7sjL._AC_UY545_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/moby-dick-melville-herman/1110282307?ean=9781593080181",
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      alreadyRead: true
+    },
+    {
+      img:
+        "https://m.media-amazon.com/images/I/91RUXhkXchL._AC_UY545_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/schindlers-list-thomas-keneally/1102298238?ean=9780671880316",
+      title: "Schindler’s List",
+      author: "Thomas Keneally",
+      alreadyRead: true
+    },
+    {
+      img: "https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg",
+      linkToRead:
+        "barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-minalima-design/1136406156?ean=9781338596700",
+      title: "Harry Potter and the Philosopher’s Stone",
+      author: "J.K. Rowling",
+      alreadyRead: true
+    },
+    {
+      img:
+        "https://m.media-amazon.com/images/I/81af+MCATTL._AC_UY545_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/the-great-gatsby-sparknotes/1104681310?ean=9781411469570",
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      alreadyRead: true
+    },
+    {
+      img:
+        "https://m.media-amazon.com/images/I/71cMat1+a+L._AC_UL800_FMwebp_QL65_.jpg",
+      linkToRead:
+        "https://www.barnesandnoble.com/w/barnes-noble-leatherbound-classics-pride-and-prejudice-jane-austen/1106658784?ean=9781435159631",
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      alreadyRead: false
+    }
+  ];
+  
+  let todoBooks = document.querySelector(".todo-books");
+  let doneBooks = document.querySelector(".done-books");
+  
+  function sortBooks(books) {
+    let readed = [];
+    let unReaded = [];
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].alreadyRead) {
+        readed.push(books[i]);
+      } else {
+        unReaded.push(books[i]);
+      }
+    }
+    return { readed, unReaded };
+  }
+  // console.log(sortBooks(myBooks));
+  let { readed, unReaded } = sortBooks(myBooks);
+  
+  drawBookList(unReaded, todoBooks);
+  drawBookList(readed, doneBooks);
+  
+  function drawBookList(list, container) {
+    //list = uread & read/ cont = node
+    container.innerHTML = "";
+    for (let i = 0; i < list.length; i++) {
+      let book = list[i];
+      var boxNode = document.createElement("div");
+      boxNode.classList.add("oneBox");
+  
+      var image = document.createElement("img");
+      image.classList.add("bookCover");
+      image.src = book.img;
+  
+      var title = document.createElement("a");
+      title.classList.add("title");
+      title.setAttribute("href", book.linkToRead);
+      title.textContent = book["title"];
+  
+      var author2 = document.createElement("p");
+      author2.classList.add("author");
+      author2.textContent = "by " + book["author"];
+  
+      image.addEventListener("click", function () {
+        book.alreadyRead = !book.alreadyRead;
+        list.splice(i, 1); // unReaded
+        if (list !== unReaded) {
+          unReaded.unshift(book);
+        } else {
+          readed.unshift(book);
+        }
+        drawBookList(unReaded, todoBooks);
+        drawBookList(readed, doneBooks);
+      });
+  
+      boxNode.appendChild(image);
+      boxNode.appendChild(title);
+      boxNode.appendChild(author2);
+      container.appendChild(boxNode);
+    }
+  }
+  
+  // var = you can get acces to them from the other scope and you can simply declare with same var.Name
+  // let vars can be accessible in global and local scopes, and it will not give allowed to create a same variable, but it can be chageble in future.
+  //const it is constant variable, so it is more securable declaring of variable, BUT, if you decided to change it later, it will not give that oportunity, so u need to change your declarition, anfd than make some changes.
+  
